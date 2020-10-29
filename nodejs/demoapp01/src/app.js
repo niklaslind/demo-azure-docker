ade56394e9f1137d1bf7726e7f3921c0f8b00dd3
Niklas Lind <niklas.johan.lind@gmail.com>
2020-10-29 12:33:52 +0100

    scripts/deploy_alias.sh describes working ty.create and ty.deploy

---
+++ nodejs/demoapp01/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      app = express();
+
+app.use(bodyParser.json());
+
+app.get('/healthcheck', (req, res) => {
+  res.json({
+    demoapp01: {
+      status: 'ok'
+    }
+  });
+});
+
+
+const port = process.env.DEMOAPP01_PORT || process.env.PORT || 3001;
+if (port) {
+  app.listen(
+    port,
+    () => { console.log(`deemoapp01 listening to port: ${port}`); }
+  );
+} else {
+  console.log(`Environment variable \`TAMBUR_CASE_PORT' nor \`PORT' set. Exiting!`);
+}
+
+
+
---
+++ scripts/deploy_alias.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+
+
+export DOCKER_USERNAME=niklaslind
+export DOCKER_PASSWORD=${DOCKER_PASSWORD}
+export RESOURCE_GROUP=ty_ResourceGroup_1
+
+ty.login() {
+    echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin #-p $DOCKER_PASSWORD
+    docker login azure 
+}
+
+ty.init.createResourceGroup() {
+    az group create --name ${RESOURCE_GROUP} --location "North Europe"
+    az appservice plan create --name ty_servicePlan_1 --resource-group ${RESOURCE_GROUP} --sku S1 --is-linux
+}
+
+ty.init.createContext() {
+    docker context create aci ty_test01 --resource-group ${RESOURCE_GROUP}
+    docker context create aci ty_prod --resource-group ${RESOURCE_GROUP}   
+    docker context list
+}
+
+
+ty.deploy.push() {
+    docker-compose push
+}
+
+
+ty.deploy.local() {
+    docker context use default
+    docker context list
+    docker-compose up -d
+    docker ps
+}
+
+ty.deploy.test01() {
+    docker context use ty_test01
+    docker context list
+    docker compose up 
+    docker ps
+}
+
+ty.deploy.prod() {
+    docker context use ty_prod
+    docker context list
+    docker compose up 
+    docker ps
+}
